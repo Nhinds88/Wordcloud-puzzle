d7a6e372f385ea1b72ed7c64c4d4a916dcf05bd7
Nhinds88 <nhinds@csumb.edu>
2022-03-15 16:16:37 -0700

    landing page rework and game route setup

--- flask_test/app.py
+++ flask_test/app.py
@@ -6,16 +6,16 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "funsecrects"
 
-@app.route("/hello")
+@app.route("/")
 def index():
     # flash("what's your name?")
     choice = random.choice([1,2,3])
     wcPuzzle(choice)
     return render_template("index.html")
     
-@app.route("/greet", methods=["POST", "GET"])
-def greeter():
-    flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+@app.route("/cluzzle", methods=["POST", "GET"])
+def cluzzle():
+    # flash("Hi " + str(request.form['name_input']) + ", great to see you!")
     return render_template("index.html")
     
 if __name__ == '__main__':
